ba3a718c63016d5d937a70e4fc292bf101e559ba
Sergio Mendoza <smendoza787@gmail.com>
2017-09-28 22:50:28 -0700

    fetched data from api and inserted into pg database with promises and knex library

--- promises-example.js
+++ promises-example.js
@@ -18,10 +18,36 @@ const pg = require('knex')({
 
 var queryUSDAFarmersMarkets = (zipCode) => {
   return new Promise((resolve, reject) => {
-    const baseUrl = 'http://search.ms.usda.gov/farmersmarkets/v1/data.svc/zipSearch?zip=';
+    const baseUrl = 'http://search.ams.usda.gov/farmersmarkets/v1/data.svc/zipSearch?zip=';
     request(baseUrl + zipCode, (err, resp, body) => {
       if (err) reject(err);
       resolve(body);
     });
   });
 };
+
+Promise.resolve(knex.schema.createTableIfNotExists('markets', (table) => {
+  table.string('id');
+  table.string('marketname');
+}))
+  .then(() => {
+    console.log('Fetching list of markets from USDA server...');
+    return queryUSDAFarmersMarkets(10004).then((respBody) => {
+      return JSON.parse(respBody).results;
+    });
+  })
+  .then((markets) => {
+    console.log('Inserting query results into the db...');
+    return Promise.all(markets.map((market) => {
+      return knex('markets').insert(market);
+    }));
+  })
+  .then((result) => {
+    return knex.select('*').from('markets').then((result) => {
+      console.log('Now our markets table has data in it!');
+      console.log('Contents of markets table: ', result);
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
